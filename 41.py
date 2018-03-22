1aac4e153bd028f598e11b48a334d9b6508b444d
djkim <harhyom9189@gmail.com>
2018-03-22 15:42:12 +0900

    print_result함수 리스트 정렬 추가

--- 41.py
+++ 41.py
@@ -1,3 +1,5 @@
+
+
 def get_input_name(file_name):
     with open(file_name, "w") as f:
         while True:
@@ -7,18 +9,24 @@ def get_input_name(file_name):
             else:
                 f.write(cur_name + "\n")
 
+
 def print_result(file_name):
     with open(file_name, "r") as f:
         name_list = f.readlines()
-        print "\nTotal of {} names".format(len(name_list))
-        print "-----------------\n"
-        for i in name_list:
-            print i.replace("\n", "")
+
+    name_list.sort()
+
+    print "\nTotal of {} names".format(len(name_list))
+    print "-----------------\n"
+    for i in name_list:
+        print i.replace("\n", "")
+
 
 def main():
     get_input_name("name_list")
     print_result("name_list")
     return
+
 
 if __name__ == "__main__":
     main()
