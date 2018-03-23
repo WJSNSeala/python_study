e528e99d3f107b15d0f3e8cfb85e3cf3d4cb8f66
djkim <harhyom9189@gmail.com>
2018-03-23 23:16:26 +0900

    curtime web service by flask

---
+++ 52_2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+filename: time alarm service flask
+date: 2018-03-23
+purpose: flask web server in local
+
+"""
+from flask import Flask
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/application/json')
+def cur_time():
+    date = str(datetime.datetime.now())
+    date_dict = {}
+    date_dict["curtime"] = date
+    json = str(date_dict)
+    return json
+
+
+if __name__ == "__main__":
+    app.run()
