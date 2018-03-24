904ef6243478ef208140e259910b6285b3de5a88
djkim <harhyom9189@gmail.com>
2018-03-24 16:54:31 +0900

    각 부분 문자열 구성 함수화

--- 37.py
+++ 37.py
@@ -1,10 +1,19 @@
 import random
 
+
+def get_random_str(num, char_set):
+    rand_str = ""
+    while num > 0:
+        rand_str += char_set[random.randrange(len(char_set))]
+        num -= 1
+    return rand_str
+
+
 alpha_list = list("abcdefghijklmnopqrstuvwxyz")
 numeric_list = list("0123456789")
 special_list = list("~!@#$%^&*()?/><")
 
-minumum_length = int(raw_input("What's the minumun length? "))
+minimum_length = int(raw_input("What's the minumun length? "))
 num_special = int(raw_input("How many special characters? "))
 num_numeric = int(raw_input("How many number? "))
 
@@ -12,20 +21,12 @@ num_str = ""
 spec_str = ""
 alpha_str = ""
 
-for i in range(num_numeric):
-    num_str += numeric_list[random.randrange(len(numeric_list))]
+num_str = get_random_str(num_numeric, numeric_list)
+spec_str = get_random_str(num_special, special_list)
 
-for i in range(num_special):
-    spec_str += special_list[random.randrange(len(special_list))]
+max_length = random.randrange(minimum_length, minimum_length + 10)
 
-max_leng = random.randrange(minumum_length, minumum_length + 10)
-
-for i in range(max_leng - num_special - num_numeric):
-    alpha_str += alpha_list[random.randrange(len(alpha_list))]
-
-print num_str
-print spec_str
-print alpha_str
+alpha_str = get_random_str(max_length - num_special - num_numeric, alpha_list)
 
 result = list(alpha_str)
 
